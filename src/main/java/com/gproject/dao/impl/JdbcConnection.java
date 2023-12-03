19ae2c72e8230b81e282bc0849465ea7670e038c
Anthony Kolb <47867707+AntonKolbaska@users.noreply.github.com>
2023-12-03 18:05:45 +0300

    Troubleshooting: update JDBC user credentials

--- src/main/java/com/gproject/dao/impl/JdbcConnection.java
+++ src/main/java/com/gproject/dao/impl/JdbcConnection.java
@@ -11,10 +11,10 @@ public class JdbcConnection {
 //    private static String URL = "jdbc:postgresql://localhost:5433/GP01";
     private static String URL = "jdbc:mysql://192.168.100.32:3306/TomcatDB";
 //    private static String USER = "postgres";
-    private static String USER = "chief";
+    private static String USER = "tomcat";
     //chief
 //    private static String PASSWORD = "root";
-    private static String PASSWORD = "Chief_217";
+    private static String PASSWORD = "Tomcat_217";
     //Chief_217
     private static final Logger LOGGER =
             Logger.getLogger(JdbcConnection.class.getName());
